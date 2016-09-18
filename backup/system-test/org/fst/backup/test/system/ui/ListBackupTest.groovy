81869ae52ab1485f66842b34033213b583f85b29
FrankStephan <fr.stephan@hotmail.de>
2016-09-18 20:21:52 +0200

    Continued system test for ui classes

--- backup/system-test/org/fst/backup/test/system/ui/ListBackupTest.groovy
+++ backup/system-test/org/fst/backup/test/system/ui/ListBackupTest.groovy
@@ -3,8 +3,15 @@ package org.fst.backup.test.system.ui
 import static org.junit.Assert.*
 import groovy.swing.SwingBuilder
 
+import javax.swing.DefaultListModel
+import javax.swing.DefaultListSelectionModel
+import javax.swing.DefaultSingleSelectionModel
+import javax.swing.text.PlainDocument
+
 import org.fst.backup.test.AbstractTest
 import org.fst.backup.ui.CommonViewModel
+import org.fst.backup.ui.frame.choose.BackupDirectoryChooser
+import org.fst.backup.ui.frame.choose.IncrementsList
 import org.fst.backup.ui.frame.create.CreateBackupButton
 import org.fst.backup.ui.frame.create.SourceFileChooser
 import org.fst.backup.ui.frame.create.TargetFileChooser
@@ -24,19 +31,24 @@ class ListBackupTest extends AbstractTest {
 		}
 
 		CommonViewModel commonViewModel = new CommonViewModel()
+		commonViewModel.tabsModel = new DefaultSingleSelectionModel()
+		commonViewModel.incrementsListModel = new DefaultListModel<>()
+		commonViewModel.incrementsListSelectionModel = new DefaultListSelectionModel()
+		commonViewModel.consoleDocument = new PlainDocument()
 		SwingBuilder swing = new SwingBuilder()
 
 		def sfc = new SourceFileChooser().createComponent(commonViewModel, swing)
 		def tfc = new TargetFileChooser().createComponent(commonViewModel, swing)
-		def button = new CreateBackupButton().createComponent(swing, {})
-
+		def button = new CreateBackupButton().createComponent(commonViewModel, swing, {})
+		def chooser = new BackupDirectoryChooser().createComponent(commonViewModel, swing)
 
 		sfc.setCurrentDirectory(sourceDir)
 		tfc.setCurrentDirectory(targetDir)
-
-
 		button.doClick()
 
-		fail()
+		chooser.selectedFile = targetDir
+
+		def list = new IncrementsList().createComponent(commonViewModel, swing)
+		println list.getModel()*.increment
 	}
 }
