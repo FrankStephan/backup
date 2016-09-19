8364b56e413f65975881a68375a9c55b829de051
Frank Stephan <frank.stephan@nterra.com>
2016-09-19 21:45:50 +0200

    Added test for IncrementsList

--- backup/src/org/fst/backup/ui/frame/choose/IncrementsList.groovy
+++ backup/src/org/fst/backup/ui/frame/choose/IncrementsList.groovy
@@ -3,6 +3,7 @@ package org.fst.backup.ui.frame.choose
 import groovy.swing.SwingBuilder
 
 import javax.swing.JList
+import javax.swing.ListSelectionModel;
 
 import org.fst.backup.ui.CommonViewModel
 
@@ -12,7 +13,8 @@ class IncrementsList {
 	JList createComponent(CommonViewModel commonViewModel, SwingBuilder swing) {
 		JList list = swing.list(
 				model: commonViewModel.incrementsListModel,
-				selectionModel: commonViewModel.incrementsListSelectionModel
+				selectionModel: commonViewModel.incrementsListSelectionModel,
+				selectionMode: ListSelectionModel.SINGLE_SELECTION
 				)
 		return list
 	}
