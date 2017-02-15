b3b04b149db61129ec5f978d8d6a33c277186146
FrankStephan <fr.stephan@hotmail.de>
2017-02-15 23:44:29 +0100

    Created service to copy songs and create songs index file

--- shuffle-my-music/src/org/fst/shuffle_my_music/DirectoryService.groovy
+++ shuffle-my-music/src/org/fst/shuffle_my_music/DirectoryService.groovy
@@ -1,9 +1,19 @@
 package org.fst.shuffle_my_music
 
+import java.nio.file.Files
 import java.nio.file.Path
 
 class DirectoryService {
 
 	boolean createDirAndIndexList(List<Path> songs, Path targetDir) {
+		if (Files.exists(targetDir)) {
+			targetDir.toFile().deleteDir()
+		}
+		Files.createDirectories(targetDir)
+		songs.each {
+			Files.copy(it, targetDir.resolve(it.fileName))
+		}
+		Files.createFile(targetDir.resolve('songs.txt'))
+		Files.write(targetDir.resolve('songs.txt'), songs*.toString())
 	}
 }
--- shuffle-my-music/test/org/fst/shuffle_my_music/DirectoryServiceTest.groovy
+++ shuffle-my-music/test/org/fst/shuffle_my_music/DirectoryServiceTest.groovy
@@ -2,9 +2,65 @@ package org.fst.shuffle_my_music
 
 import static org.junit.Assert.*
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class DirectoryServiceTest extends GroovyTestCase {
 
-	void testTargetDirIsClearedOrCreated() {
-		fail("Not yet implemented")
+	Path testDir = Paths.get(this.getClass().getSimpleName())
+	Path targetDir = testDir.resolve('targetDir')
+	Path mediaLibraryDir = testDir.resolve('mediaLibrary')
+
+	void setUp() {
+		super.setUp()
+		mediaLibraryDir.toFile().mkdirs()
+		FileTreeBuilder ftb = new FileTreeBuilder(mediaLibraryDir.toFile())
+		ftb {
+			a0 {
+				a1 { 'a2.mp3'('') } // 0
+				b1 { 'b2.jpg'('') } // 1
+			}
+			'a0.mp3'('') // 2
+			'b0.mp3'('') // 3
+			'c0.jpg'('') // 4
+		}
+	}
+
+	void tearDown() {
+		super.tearDown()
+		testDir.toFile().deleteDir()
+	}
+
+	void testTargetDirIsCreatedIfNotExisiting() {
+		assert !Files.exists(targetDir)
+		new DirectoryService().createDirAndIndexList([], targetDir)
+		assert Files.exists(targetDir)
+		assert Files.isDirectory(targetDir)
+	}
+
+	void testAllSongsAreCopied() {
+		List songs = [path('a0.mp3'), path('a0/a1/a2.mp3')]
+		new DirectoryService().createDirAndIndexList(songs, targetDir)
+		assert (targetDir.toFile().list() as List).containsAll(['a0.mp3', 'a2.mp3'])
+	}
+
+	void testTargetDirIsClearedIfExisting() {
+		List songs = [path('a0.mp3')]
+		new DirectoryService().createDirAndIndexList(songs, targetDir)
+		songs = [path('a0/a1/a2.mp3')]
+		new DirectoryService().createDirAndIndexList([], targetDir)
+		assert !(targetDir.toFile().list() as List).contains('a0.mp3')
+	}
+
+	void testSongListIsCreated() {
+		List songs = [path('a0.mp3'), path('a0/a1/a2.mp3')]
+		new DirectoryService().createDirAndIndexList(songs, targetDir)
+		assert (targetDir.toFile().list() as List).contains('songs.txt')
+		assert songs*.toString() == Files.readAllLines(targetDir.resolve('songs.txt'))
+	}
+
+	private Path path(String path) {
+		return mediaLibraryDir.resolve(path)
 	}
 }
