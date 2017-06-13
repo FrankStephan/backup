6f88b97af62efe6d0dd4747759bf08d5a0116058
Frank Stephan <frank.stephan@nterra.com>
2017-06-13 17:03:16 +0200

    Started project for PERT estimation gadget

---
+++ perte/src/main/java/org/fst/perte/PertEstimationService.groovy
@@ -0,0 +1,9 @@
+package org.fst.perte
+
+class PertEstimationService {
+	
+	float pert(float optimistic, float realisitic, float pessimistic, float probability) throws IllegalArgumentException {
+		
+	}
+
+}
