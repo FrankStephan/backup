6dccdf6987cbad0b808f5fbc08093d2fa2f009f0
FrankStephan <fr.stephan@hotmail.de>
2017-07-05 23:10:27 +0200

    Started library for smb connection to shuffle-my-music files

---
+++ shuffle-my-music-smb/src/test/java/com/frozen_foo/shuffle_my_music_smb/SmbAccessTest.groovy
@@ -0,0 +1,12 @@
+package com.frozen_foo.shuffle_my_music_smb
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+class SmbAccessTest {
+
+	@Test
+	public void testIsAvailableIfSongsCanBeAccessed() {
+	}
+}
