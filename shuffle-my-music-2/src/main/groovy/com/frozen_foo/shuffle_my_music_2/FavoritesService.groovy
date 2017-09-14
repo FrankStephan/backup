53c3a85752d3ace67821dc02f189d8a544e7a2db
FrankStephan <fr.stephan@hotmail.de>
2017-09-14 23:10:21 +0200

    Continued implementation of favorites service

--- shuffle-my-music-2/src/main/groovy/com/frozen_foo/shuffle_my_music_2/FavoritesService.groovy
+++ shuffle-my-music-2/src/main/groovy/com/frozen_foo/shuffle_my_music_2/FavoritesService.groovy
@@ -1,5 +1,7 @@
 package com.frozen_foo.shuffle_my_music_2
 
+import groovy.xml.MarkupBuilder
+
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -8,25 +10,64 @@ import java.nio.file.Paths
 class FavoritesService {
 
 
-	IndexEntry[] addFavorites(String targetDirPath, IndexEntry[] newFavorites) {
-		Path favoritesFile = createFavoritesFileIfNecessary(targetDirPath)
+    IndexEntry[] addFavorites(String targetDirPath, IndexEntry[] newFavorites) {
+        Path favoritesFile = createOrGetFile(targetDirPath)
+        List<IndexEntry> favorites = calculateResultingFavorites(favoritesFile, newFavorites)
+        writeFavoritesToFile(favoritesFile, favorites)
+        return favorites
+    }
 
-		return null
-	}
+    private Path createOrGetFile(String targetDirPath) {
+        Path targetDir = Paths.get(targetDirPath).toAbsolutePath()
+        Path favoritesFile = targetDir.resolve('favorites.xml')
+        if (!Files.exists(favoritesFile)) {
+            Files.createDirectories(targetDir)
+            Files.createFile(favoritesFile)
+        }
+        return favoritesFile
+    }
 
-	private Path createFavoritesFileIfNecessary(String targetDirPath) {
-		Path targetDir = Paths.get(targetDirPath).toAbsolutePath()
-		Files.createDirectories(targetDir)
-		Path favoritesFile = targetDir.resolve('favorites.xml')
-		if (!Files.exists(favoritesFile)) {
-			Files.createFile(favoritesFile)
-		}
-		return favoritesFile
-	}
+    private List<IndexEntry> calculateResultingFavorites( Path favoritesFile, IndexEntry[] newFavorites) {
+        List<IndexEntry> favorites = readFavoritesFromFile(favoritesFile) as List
+        newFavorites.each { IndexEntry it ->
+            favorites << it
+        }
+        removeDuplicates(favorites)
+    }
 
-	private void appendFavorite() {
-	}
+    private List<IndexEntry> removeDuplicates(List<IndexEntry> favorites) {
+        favorites.unique()
+    }
 
-	IndexEntry[] loadFavorites(String targetDirPath) {
-	}
+    private String writeFavoritesToFile(Path favoritesFile, List<IndexEntry> favorites) {
+        favoritesFile.withWriter('UTF-8') { favoritesFileWriter ->
+            new MarkupBuilder(favoritesFileWriter).favorites(timestamp: new Date().getDateTimeString()) {
+                for (IndexEntry favorite : favorites) {
+                    song(title: favorite.fileName, favorite.path)
+                }
+            }
+        }
+    }
+
+    IndexEntry[] loadFavorites(String targetDirPath) {
+        Path favoritesFile = createOrGetFile(targetDirPath)
+        return readFavoritesFromFile(favoritesFile)
+
+    }
+
+    private Object readFavoritesFromFile(Path favoritesFile) {
+        String xmlString = favoritesFile.toFile().getText('UTF-8')
+        if (!xmlString.isEmpty()) {
+            def favoritesXml = new XmlSlurper().parseText(xmlString)
+
+            IndexEntry[] favorites = new IndexEntry[favoritesXml.song.size()]
+            favoritesXml.song.eachWithIndex { def song, int i ->
+                favorites[i] = new IndexEntry(fileName: song.@title, path: song)
+            }
+            return favorites
+        } else {
+            return []
+        }
+    }
+
 }
--- shuffle-my-music-2/src/test/groovy/com/frozen_foo/shuffle_my_music_2/FavoritesServiceTest.groovy
+++ shuffle-my-music-2/src/test/groovy/com/frozen_foo/shuffle_my_music_2/FavoritesServiceTest.groovy
@@ -1,5 +1,8 @@
 package com.frozen_foo.shuffle_my_music_2
 
+import groovy.xml.MarkupBuilder
+import groovy.xml.MarkupBuilderHelper
+
 import static org.junit.Assert.*
 
 import java.nio.file.Files
@@ -8,59 +11,131 @@ import java.nio.file.Paths
 
 class FavoritesServiceTest extends GroovyTestCase {
 
-	Path testPath = Paths.get(this.getClass().getSimpleName())
+	Path testPath
+	Path favoritesFilePath
 
-	Path favoritesFilePath = testPath.resolve('favorites.xml')
 	IndexEntry[] newFavorites
 
 	IndexEntry[] favorites
 
+	@Override
+	protected void setUp() {
+		testPath = Paths.get(this.getClass().getSimpleName())
+		favoritesFilePath = testPath.resolve('favorites.xml')
+	}
+
 	void testCreatesNewFileIfNotExisting() {
+		favoritesFilePath = favoritesFilePath.toAbsolutePath()
 		assert !Files.exists(favoritesFilePath)
 		invokeAdd()
 		assert Files.exists(favoritesFilePath)
 	}
 
-	void testAppendsNewEntries() {
-		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', 'dir1/song1.mp3')
-		IndexEntry favorite2 = new IndexEntry(fileName: 'song2.mp3', 'dir2/song2.mp3')
+	void testResolvesAbsolutePathForFavoritesFile() {
+		assert !Files.exists(favoritesFilePath)
+		invokeAdd()
+		assert Files.exists(favoritesFilePath.toAbsolutePath())
+	}
+
+	void testAppendsNewEntry() {
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+
+		newFavorites = [favorite1]
+		invokeAdd()
+
+		invokeLoad()
+		assert [favorite1]== favorites
+	}
+
+	void testAppendsNewEntries1() {
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+		IndexEntry favorite2 = new IndexEntry(fileName: 'song2.mp3', path: 'dir2/song2.mp3')
 
 		newFavorites = [favorite1]
 		invokeAdd()
 
 		newFavorites = [favorite2]
-		invokeLoad()
+		invokeAdd()
 
+		invokeLoad()
 		assert [favorite1, favorite2]== favorites
 	}
 
+	void testAppendsNewEntries2() {
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+		IndexEntry favorite2 = new IndexEntry(fileName: 'song2.mp3', path: 'dir2/song2.mp3')
+
+		newFavorites = [favorite1, favorite2]
+		invokeAdd()
+
+		invokeLoad()
+		assert [favorite1, favorite2]== favorites
+	}
+
+	void testAppendsNewEntries3() {
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+		IndexEntry favorite2 = new IndexEntry(fileName: 'song2.mp3', path: 'dir2/song2.mp3')
+		IndexEntry favorite3 = new IndexEntry(fileName: 'song3.mp3', path: 'dir3/song3.mp3')
+		IndexEntry favorite4 = new IndexEntry(fileName: 'song4.mp3', path: 'dir4/song4.mp3')
+
+		newFavorites = [favorite1, favorite2]
+		invokeAdd()
+
+		newFavorites = [favorite3, favorite4]
+		invokeAdd()
+
+		invokeLoad()
+		assert [favorite1, favorite2, favorite3, favorite4]== favorites
+	}
+
 	void testReturnsAppendedEntries() {
-		fail()
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+		IndexEntry favorite2 = new IndexEntry(fileName: 'song2.mp3', path: 'dir2/song2.mp3')
+
+		newFavorites = [favorite1, favorite2]
+		assert invokeAdd() == newFavorites
 	}
 
 	void testSkipsDuplicates() {
-		fail()
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+		IndexEntry favorite2 = new IndexEntry(fileName: 'song2.mp3', path: 'dir2/song2.mp3')
+		IndexEntry favorite3 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1/song1.mp3')
+		IndexEntry favorite4 = new IndexEntry(fileName: 'song2.mp3', path: 'dir1000/song2.mp3')
+		IndexEntry favorite5 = new IndexEntry(fileName: 'song9.mp3', path: 'dir1/song1.mp3')
+
+		newFavorites = [favorite1, favorite2, favorite3]
+		invokeAdd()
+
+		newFavorites = [favorite3, favorite4, favorite5]
+		invokeAdd()
+
+		invokeLoad()
+		assert [favorite1, favorite2, favorite4, favorite5]== favorites
 	}
 
-	void containsRelativePaths() {
-		fail()
+	void testContainsRelativePaths() {
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1\\song1.mp3')
+		notYetImplemented()
 	}
 
-	void containsUpdateTimestamp() {
-		fail()
+	void testHandlesUTF8() {
+		IndexEntry favorite1 = new IndexEntry(fileName: 'song1.mp3', path: 'dir1\\äöß??_.mp3')
+		notYetImplemented()
 	}
 
-	private void invokeAdd() {
-		new FavoritesService().addFavorites(testPath.toString(), newFavorites)
+	private IndexEntry[] invokeAdd() {
+		return new FavoritesService().addFavorites(testPath.toString(), newFavorites)
 	}
 
 	private void invokeLoad() {
-		favorites = new FavoritesService().loadFavorites(testPath)
+		favorites = new FavoritesService().loadFavorites(testPath.toString())
 	}
 
 	@Override
 	protected void tearDown() throws Exception {
-		testPath.toFile().deleteDir()
+		testPath.deleteDir()
 		super.tearDown()
 	}
+
+
 }
