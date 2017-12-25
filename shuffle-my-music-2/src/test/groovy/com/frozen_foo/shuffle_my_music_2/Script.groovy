2f33589a1ef47be44b157fe32fa714b65d83e882
FrankStephan <fr.stephan@hotmail.de>
2017-12-25 23:45:34 +0100

    Modified script for index creation to write the required time in minutes

--- shuffle-my-music-2/src/test/groovy/com/frozen_foo/shuffle_my_music_2/Script.groovy
+++ shuffle-my-music-2/src/test/groovy/com/frozen_foo/shuffle_my_music_2/Script.groovy
@@ -2,7 +2,7 @@ package com.frozen_foo.shuffle_my_music_2
 
 import java.nio.file.Paths
 
-import com.frozen_foo.shuffle_my_music_2.impl.CreateIndexService;
+import com.frozen_foo.shuffle_my_music_2.impl.CreateIndexService
 
 long millis = System.currentTimeMillis()
 
@@ -10,7 +10,7 @@ String mediaLibraryPath = 'X:/Media/Musik'
 new CreateIndexService().createIndex(Paths.get(mediaLibraryPath))
 
 File time = new File(mediaLibraryPath, 'time.txt')
-time << ((System.currentTimeMillis() - millis) / 1000)
+time << (((System.currentTimeMillis() - millis) / 60000) + ' min')
 time << System.lineSeparator()
 
 ('cmd /c shutdown.exe -s -t 10').execute()
