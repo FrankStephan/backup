c170e2dcc44eaf0817152bf11dae86b6b42a5adc
FrankStephan <fr.stephan@hotmail.de>
2018-05-05 22:59:44 +0200

    app now keeps the last played song and position to continue playback when the user returns

---
+++ shuffle-my-music-app/app/src/main/java/com/frozen_foo/shuffle_my_music_app/list_player/PlayState.java
@@ -0,0 +1,8 @@
+package com.frozen_foo.shuffle_my_music_app.list_player;
+
+/**
+ * Created by Frank on 17.04.2018.
+ */
+
+public class PlayState {
+}
---
+++ shuffle-my-music-app/app/src/main/java/com/frozen_foo/shuffle_my_music_app/mediaplayer/PlayStateAccess.java
@@ -0,0 +1,8 @@
+package com.frozen_foo.shuffle_my_music_app.mediaplayer;
+
+/**
+ * Created by Frank on 15.04.2018.
+ */
+
+public class PlayStateAccess {
+}
